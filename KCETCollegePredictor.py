7196e437ae9ee261204374d8226f58e80e06530e
Vishnu Sastry H K <hkvsastry3300@gmail.com>
2021-06-04 23:34:12 +0530

    Update KCETCollegePredictor.py

--- KCETCollegePredictor.py
+++ KCETCollegePredictor.py
@@ -6,7 +6,7 @@ image = Image.open('BranchCode.png')
 
 
 col_names = ["CETCode",	"College" ,"Location",	"Branch",	"1G",	"1K",	"1R", 	"2AG",	"2AK",	"2AR",	"2BG",	"2BK",	"2BR",	"3AG",	"3AK",	"3AR",	"3BG",	"3BK",	"3BR",	"GM",	"GMK",	"GMR",	"SCG",	"SCK",	"SCR",	"STG",	"STK",	"STR"]
-df = pd.read_csv("https://github.com/VishnuSastryHK/KCETCollegePredictor/blob/master/CET_Database_Final2019.csv", index_col=False, encoding='iso-8859-1', 
+df = pd.read_csv("https://github.com/VishnuSastryHK/KCETCollegePredictor/blob/master/CET_Database_Final2019.csv",  encoding='iso-8859-1', 
                         warn_bad_lines=True, error_bad_lines=False)#names=col_names,)
 outputdframe = pd.DataFrame(columns = ['Branch', 'College', 'Location', 'CET Code','Cutoff'])
 pd.set_option('colheader_justify', 'left')
