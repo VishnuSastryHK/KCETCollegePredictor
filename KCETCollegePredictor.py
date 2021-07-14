8373b9d87b794ee7f00cd93b8d553107c30f8b8f
Vishnu Sastry H K <hkvsastry3300@gmail.com>
2021-07-14 17:16:52 +0530

    Update KCETCollegePredictor.py

--- KCETCollegePredictor.py
+++ KCETCollegePredictor.py
@@ -352,8 +352,8 @@ for i in Branch_List:
                                 ignore_index = True)
             
 
+df2=outputdframe.style.set_properties(**{‘text-align’: ‘left’}).set_table_styles([dict(selector=‘th’, props=[(‘text-align’, ‘left’)])])
 
-df2=outputdframe.style.set_properties(**{‘text-align’: ‘left’}).set_table_styles([dict(selector='th', props=[('text-align', 'left')])])
 
 
 if(len(Branch_List)>0):
