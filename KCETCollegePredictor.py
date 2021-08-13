3633564e7f4edb8867d05ef60627b9318275b329
Vishnu Sastry H K <hkvsastry3300@gmail.com>
2021-08-13 21:55:40 +0530

    Update KCETCollegePredictor.py

--- KCETCollegePredictor.py
+++ KCETCollegePredictor.py
@@ -5,7 +5,6 @@ import io
 from PIL import Image
 image = Image.open('BranchCode.png')
 
-
 col_names = ["CETCode",	"College" ,"Location",	"Branch",	"1G",	"1K",	"1R", 	"2AG",	"2AK",	"2AR",	"2BG",	"2BK",	"2BR",	"3AG",	"3AK",	"3AR",	"3BG",	"3BK",	"3BR",	"GM",	"GMK",	"GMR",	"SCG",	"SCK",	"SCR",	"STG",	"STK",	"STR"]
 #df = pd.read_csv("https://github.com/VishnuSastryHK/KCETCollegePredictor/blob/master/CET_Database_Final2019.csv", sep='[:,|_()]\s+',names=col_names, header=None, 
 #delimiter=',' , engine="python")#)#,sep=r'\s*,\s*', sep="\s+|;|:", error_bad_lines=False)# names=col_names)
