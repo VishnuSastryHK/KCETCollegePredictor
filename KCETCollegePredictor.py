974a48a8978c0b2700cd376f654163a6d4dd3a2b
Vishnu Sastry H K <hkvsastry3300@gmail.com>
2021-09-28 10:55:00 +0530

    Update KCETCollegePredictor.py

--- KCETCollegePredictor.py
+++ KCETCollegePredictor.py
@@ -14,7 +14,7 @@ st.set_page_config(
     menu_items={
         'Get Help': 'https://www.extremelycoolapp.com/help',
         'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
+        'About': "This is a header. This is an *extremely* cool app!"
     }
 )
 
