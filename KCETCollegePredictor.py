7c84fe0c502b17602e5ea07a6c1aa7da3c480c89
Vishnu Sastry H K <hkvsastry3300@gmail.com>
2021-09-28 11:13:13 +0530

    Update KCETCollegePredictor.py

--- KCETCollegePredictor.py
+++ KCETCollegePredictor.py
@@ -6,17 +6,11 @@ from PIL import Image
 image = Image.open('BranchCode.png')
 
 #hamburger navbar options
-st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "This is a header. This is an *extremely* cool app!"
-    }
-)
+st.set_page_config(page_title="Ex-stream-ly Cool App",page_icon="🧊",layout="wide",initial_sidebar_state="expanded",menu_items={
+    'Get Help': 'https://www.extremelycoolapp.com/help',
+    'Report a bug': "https://www.extremelycoolapp.com/bug",
+    'About': "This is a header. This is an *extremely* cool app!"
+})
 
 
 
